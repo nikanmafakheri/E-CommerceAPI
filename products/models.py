fb270bef690963e006ecd0a364025e60cd1b250f
nikan <nikanmafakherii@gmail.com>
2025-07-22 20:04:50 +0330

    feat: add customer model

--- products/models.py
+++ products/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class category(models.Model):
   name = models.CharField(max_length= 100)
   
+  @staticmethod
+  def get_all_categories():
+    return category.objects.all()
+  
+  def __str__(self):
+    return self.name
+
+class Customer (models.Model):
+  first_name = models.CharField(max_length = 12)
+  last_name = models.CharField(max_length = 20)
+  phone_number = models.IntegerField(max_length = 11)
+  email = models.EmailField()
+  password = models.CharField(max_length = 30)
