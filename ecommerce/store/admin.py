9fa3f074a7635baf89c0b282f4f8dc8f3aa2f068
nikan <nikanmafakherii@gmail.com>
2025-07-23 16:29:10 +0330

    feat: add superuser and migrations

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Customer , category
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(category)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length= 100)
+  
+  @staticmethod
+  def get_all_categories():
+    return Category.objects.all()
+  
+  def __str__(self):
+    return self.name
+
+class Customer (models.Model):
+  first_name = models.CharField(max_length = 12)
+  last_name = models.CharField(max_length = 20)
+  phone_number = models.CharField()
+  email = models.EmailField()
+  password = models.CharField(max_length = 30)
